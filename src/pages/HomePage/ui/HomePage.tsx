f61d9644ac0d3018cd2d4300ee3feccc41a2b569
AlexTrubkina <sashatru99@gmail.com>
2024-05-28 19:12:18 +0300

    feat: add HomePage component

---
+++ src/pages/HomePage/ui/HomePage.tsx
@@ -0,0 +1,18 @@
+import { ReactElement } from "react";
+import { FormControl } from "components/FormControl";
+import { Container } from "components/Container";
+import storeInstance from "store";
+import { AutoComplete } from "components/AutoComplete";
+
+export function HomePage(): ReactElement {
+
+    return (
+        <Container>
+            <h1>Компонент Контрол</h1>
+            <FormControl store={ storeInstance } buttons={[{name: "Hello World", position: "right", callback: storeInstance.changeValue.bind(storeInstance, "Hello World!")}, {name: "Очистить", position: "right", callback: storeInstance.changeValue.bind(storeInstance, "")}]} />
+            <FormControl store={ storeInstance } buttons={[{name: "Alert", position: "right", callback: storeInstance.alertInput}, {name: "Число", position: "left", callback: storeInstance.alertNumber}]} />
+            <AutoComplete store={storeInstance} />
+        </Container>
+    
+    );
+}
