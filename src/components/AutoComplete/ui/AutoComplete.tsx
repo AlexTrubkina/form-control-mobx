3d639af08909852b24ed3eebe44bd2c05c787457
AlexTrubkina <sashatru99@gmail.com>
2024-05-28 19:40:08 +0300

    feat: add debounce for request in autocomplete

--- src/components/AutoComplete/ui/AutoComplete.tsx
+++ src/components/AutoComplete/ui/AutoComplete.tsx
@@ -1,21 +1,30 @@
 import { getCountryByName } from "api/apiService";
-import { ChangeEvent, ReactElement } from "react";
+import { ChangeEvent, ReactElement, useEffect } from "react";
 import { observer } from "mobx-react-lite";
-import { store } from "types";
+import { autoCompleteStore } from "types";
 import { AutoCompleteItem } from "./AutoCompleteItem";
 import './style.scss';
 
-export const AutoComplete = observer(({store}: {store: store}) : ReactElement => {
+export const AutoComplete = observer(({store}: {store: autoCompleteStore}) : ReactElement => {
 
     const handleChange = async (e: ChangeEvent<HTMLInputElement>) => {
         store.changeValue(e.currentTarget.value);
-        if (e.currentTarget.value) {
-            const result = await getCountryByName(e.currentTarget.value);
-            store.changeAutocomplete(result);
-        } 
-        else store.changeAutocomplete([]);
     }
 
+    useEffect(() => {
+        const delayDebounceFn = setTimeout(() => {
+            if (store.value) {
+                const sendRequest = async () => {
+                    const result = await getCountryByName(store.value);
+                    store.changeAutocomplete(result);
+                }
+                sendRequest();
+                console.log("request sended");
+                
+            } 
+        }, 2000)
+        return () => clearTimeout(delayDebounceFn)
+        }, [store.value])
     return (
         <div className="autocomplete">
             <input className="autocomplete__input" placeholder="Введие страну" type="text" onChange={handleChange} value={store.value}/>
